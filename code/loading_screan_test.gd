b6f115ec895ba554781666b04002f095d37015b9
pobop-sys <pobop32324@gmail.com>
2026-08-08 00:40:08 +0100

    Update loading_screan_test.gd

--- code/loading_screan_test.gd
+++ code/loading_screan_test.gd
@@ -6,7 +6,7 @@ func _ready() -> void:
 	$AnimationPlayer.play("loding screan")
 	$Timer.start()
 	
-# test
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
