b6cf464e78e68552ebecbc880f585bdc83aeceb1
pobop-sys <pobop32324@gmail.com>
2026-08-23 15:54:32 +0100

    dialoge code work in progress

---
+++ code/dialogue_text_resorse.gd
@@ -0,0 +1,9 @@
+extends Resource
+class_name dialoguetext
+
+@export var speaker_image: Texture
+@export var speaker_img_hframes: int = 1
+@export var speaker_img_rest_frame: int = 0
+
+#@export_multiline(var text: string) 
+@export_range(0.1, 30.0, 0.1) var text_speed: float
