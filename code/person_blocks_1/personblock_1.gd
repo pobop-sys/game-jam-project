7024585d08b733b0c1017e4e4ad811c0920227c3
pobop-sys <pobop32324@gmail.com>
2026-08-23 15:05:57 +0100

    more effects to person box
    dosnt work yet but the @exsports are there

--- code/person_blocks_1/personblock_1.gd
+++ code/person_blocks_1/personblock_1.gd
@@ -2,6 +2,11 @@ extends Node2D
 
 @export var is_telling_truth = false
 
+@export var effect_push :bool
+@export var push_direction:bool
+# push dir off = left push on = right push
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
