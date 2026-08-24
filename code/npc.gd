2861eb8ac30e6e6e8528ffbb35cfee4b2af9d046
pobop-sys <pobop32324@gmail.com>
2026-08-24 17:07:41 +0100

    daylogue added and working with text effects

--- code/npc.gd
+++ code/npc.gd
@@ -2,20 +2,28 @@ extends Node2D
 
 
 @export var npc_art: int
+@export var animation_lenth: float
+@export_multiline var text:String
 var npc_art_file_location = [
 	"res://art/npc art/test_image1.png",
 	"res://art/npc art/test_image2.png",
 	"res://art/npc art/test_image3.png"
 ]
 
-# Called when the node enters the scene tree for the first time.
+var text_panle_visible: bool = false
+ 
+
 func _ready() -> void:
+	$Control/PanelContainer/RichTextLabel.append_text(text)
+	$Control/PanelContainer/RichTextLabel.bbcode_enabled
+	$Control/PanelContainer/RichTextLabel.visible_ratio = 0
 	if npc_art > len(npc_art_file_location) or npc_art < 0:
 		npc_art = 0
 	var npc_load_art = load(npc_art_file_location[npc_art])
 	$Sprite2D.texture = npc_load_art
+	
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_player_collition_detecton_body_entered(body: Node2D) -> void:
+	print("player_enterd")
+	$AnimationPlayer.play("text_bar_apearing")
