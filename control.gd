01df62a3b4eee4a5d0af233e67e1b8bb91941024
BMXbandit10 <bmxbanditten@gmail.com>
2026-08-24 23:52:02 +0100

    finished button main menu ui

--- control.gd
+++ control.gd
@@ -1,14 +1,18 @@
 extends Control
 
 
-func _on_quit_pressed() -> void:
-	get_tree().quit()
-
 
 
 
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/test_lv.tscn")
+
+
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+
 
 
 
