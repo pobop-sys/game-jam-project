5b171955a2f014a0a215e44cf11c50b4ba91e515
pobop-sys <pobop32324@gmail.com>
2026-08-24 22:11:01 +0100

    makeing exsposition level

--- code/mystrey_guy.gd
+++ code/mystrey_guy.gd
@@ -1,9 +1,20 @@
 extends Node2D
 
+
+@export var flip: bool
 @export var npc_art: int
 @export_multiline var text:String
+@export var text_visiblity_ratio:float
 
-func _ready() -> void:#
+func _ready() -> void:
+	if flip:
+		$AnimatedSprite2D.flip_h = true
+	else:
+		$AnimatedSprite2D.flip_h = false
+		
 	$Control.visible = true
 	$Control/PanelContainer/RichTextLabel.append_text(text)
 	$Control/PanelContainer/RichTextLabel.bbcode_enabled
+
+func _process(delta: float) -> void:
+	$Control/PanelContainer/RichTextLabel.visible_ratio = text_visiblity_ratio
