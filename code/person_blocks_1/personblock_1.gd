5ffd7fdb49c3f925a6cf05c37e1e40073eb2060c
pobop-sys <pobop32324@gmail.com>
2026-08-25 21:04:44 +0100

    trying to fix person_box

--- code/person_blocks_1/personblock_1.gd
+++ code/person_blocks_1/personblock_1.gd
@@ -1,25 +1,17 @@
 extends Node2D
 
-
 @export var is_telling_truth: bool
-# push dir off = left push on = right push
-@export var effect_push :bool
-@export var push_direction:bool
-
 #art file loactions array
 const block_art_file_loactions: Array = [
 	"res://art/block_art/icon.svg"
-	
 ]
 @export var wich_block_art_file:int
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var block_art = load(block_art_file_loactions[wich_block_art_file])
 	$Sprite2D.texture = block_art
+	print(is_telling_truth)
+	
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	$StaticBody2D/CollisionShape2D.disabled = is_telling_truth
