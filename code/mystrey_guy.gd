602cd2f4a364c53d94c92207625fd7023e55946c
pobop-sys <pobop32324@gmail.com>
2026-08-27 12:53:19 +0100

    got the mg bug fiexed

--- code/mystrey_guy.gd
+++ code/mystrey_guy.gd
@@ -1,43 +1,34 @@
 extends Node2D
 
-
 @export var flip: bool
 @export var text_box_play_on_load: bool
 
-@export var text_box_hight: int
+@export var text_box_hight: int 
 @export var npc_art: int
-@export var text_visiblity_ratio:float
-@export_multiline var text:String
+@export var text_visiblity_ratio: float
+@export_multiline var text: String
 @export_multiline var notes: String
 
 func load_textbox_and_play_anim():
 	$AnimationPlayer.play("textbar_apear")
-	$AnimationPlayer.play("play_text")
+	$AnimationPlayer.queue("play_text")
 
 func _ready() -> void:
-	if !text_box_play_on_load:
-		$AnimationPlayer.play_backwards("textbar_apear")
-		print("it has gone back")
-		
-	else: $AnimationPlayer.play("textbar_apear")
+	$Control/PanelContainer/RichTextLabel.bbcode_enabled = true
+	$Control/PanelContainer/RichTextLabel.append_text(text)
 	
 	$Control.position.y = text_box_hight
+	$AnimatedSprite2D.flip_h = flip
 	
-	if flip:
-		$AnimatedSprite2D.flip_h = true
-	else:
-		$AnimatedSprite2D.flip_h = false
-		
 	$Control.visible = true
-	$Control/PanelContainer/RichTextLabel.append_text(text)
-	$Control/PanelContainer/RichTextLabel.bbcode_enabled
 	
 	if text_box_play_on_load:
-		$AnimationPlayer.play("play_text")
-
+		$AnimationPlayer.play("textbar_apear")
+		$AnimationPlayer.queue("play_text")
+	else:
+		$AnimationPlayer.play_backwards("textbar_apear")
+		
 func _process(delta: float) -> void:
 	if Globle.play_mg_textbox:
 		load_textbox_and_play_anim()
 		Globle.play_mg_textbox = false
-	
-	
